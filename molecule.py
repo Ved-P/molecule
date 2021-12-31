5f099013dae3e56d5f263b130b02bf9580418df9
Ved-P <vedprad1@gmail.com>
2021-12-31 15:52:49 -0800

    Created a function to get the number of valence electrons of an element.

--- molecule.py
+++ molecule.py
@@ -9,4 +9,23 @@
 import json
 import matplotlib
 
-formula = input("\n\n\nWelcome to Molecule! Please enter a molecular formula: ")
+# Opens the JSON file for use.
+with open("elements.json", "r", encoding="utf8") as file:
+    data = json.load(file)
+
+# Given the symbol of the element, return the number of its valence electrons.
+def get_valence_electrons(symbol):
+    for element in data["elements"]:
+        if element["symbol"] == symbol:
+            if symbol == "He":
+                return 2
+            elif 9 <= element["ypos"] <= 10:
+                return 2
+            elif 2 <= element["xpos"] <= 12:
+                return 2
+            else:
+                return element["xpos"] % 10
+
+# Gets the formula from the user.
+formula = input("\n\n\nWelcome to Molecule! Please enter a molecular formula"
+    + "(case sensitive): ")
