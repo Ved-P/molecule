a08247d6ec4f543689aab272a1ea9d4929b92fe5
Ved-P <vedprad1@gmail.com>
2021-12-31 15:15:08 -0800

    Created molecule.py.

---
+++ molecule.py
@@ -0,0 +1,7 @@
+# Molecule
+#
+# This program takes in a molecular formula and creates a Lewis diagram and a 3D
+# model of the molecule as the output.
+#
+# Author: Ved Pradhan
+# Since:  December 31, 2021
