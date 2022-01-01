fffb486666823c1049b1ceaca69ba8abdf453eb5
Ved-P <vedprad1@gmail.com>
2021-12-31 21:32:06 -0800

    Created a class to represent the individual atom.

--- molecule.py
+++ molecule.py
@@ -13,18 +13,43 @@ import matplotlib
 with open("elements.json", "r", encoding="utf8") as file:
     data = json.load(file)
 
-# Given the symbol of the element, return the number of its valence electrons.
-def get_valence_electrons(symbol):
-    for element in data["elements"]:
-        if element["symbol"] == symbol:
-            if symbol == "He":
-                return 2
-            elif 9 <= element["ypos"] <= 10:
-                return 2
-            elif 2 <= element["xpos"] <= 12:
-                return 2
-            else:
-                return element["xpos"] % 10
+# Class to represent each individual atom in the molecule.
+class Atom:
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.loose_ve = -1
+        self.sigma_bonds = -1
+        self.pi_bonds = -1
+        self.formal_charge = -1
+        self.total_ve = -1
+        self.element = self.get_element()
+        self.enegativity = self.element["electronegativity_pauling"]
+        self.expected_ve = self.get_valence_electrons()
+
+    # Retrieves the element this atom corresponds to from the JSON file.
+    def get_element(self):
+        for element in data["elements"]:
+            if element["symbol"] == self.symbol:
+                return element
+
+    # Returns the number of valence electrons the atom is expected to have.
+    def get_valence_electrons(self):
+        if self.symbol == "He":
+            return 2
+        elif 9 <= self.element["ypos"] <= 10:
+            return 2
+        elif 2 <= self.element["xpos"] <= 12:
+            return 2
+        else:
+            return self.element["xpos"] % 10
+
+    # Updates the formal charge of the atom.
+    def update_formal_charge(self):
+        self.formal_charge = self.expected_ve - self.loose_ve - self.sigma_bonds - self.pi_bonds
+
+    # Updates the total number of valence electrons, including shared ones.
+    def update_total_ve(self):
+        self.total_ve = self.loose_ve + 2 * (self.sigma_bonds + self.pi_bonds)
 
 # Gets the formula from the user.
 formula = input("\n\n\nWelcome to Molecule! Please enter a molecular formula"
